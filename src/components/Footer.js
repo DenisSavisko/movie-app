84c6bbc5840ad6cbe1273ff5d9a2f49264858690
Denis Savisko <42087984+DenisSavisko@users.noreply.github.com>
2019-04-25 03:55:57 +0300

    color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default () =>
-  <footer className='footer bg-secondary text-light'>
+  <footer className='footer text-light'>
     <div className=''>Denis Savisko, <a href='https://github.com/DenisSavisko/movie-app' target='blank'>GitHub
       <img src='https://2sxc.org/Portals/0/adam/Content/4IqBjx3pXEC7a7-fVX2GBQ/Image/github-logo.png' /></a>
     </div>
