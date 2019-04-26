eef7c269d4f65acb465398fd8835d88e28e4b4d0
Denis Savisko <42087984+DenisSavisko@users.noreply.github.com>
2019-04-26 10:19:48 +0300

    add build docs

---
+++ docs/precache-manifest.4b591d9d9c0b5451ec3e540366b4afe3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "78a64a09a87326d023ae53922e3e3f08",
+    "url": "/static/media/logo.78a64a09.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5ec5ed894fef17538a8a",
+    "url": "/static/js/main.82e0eff0.chunk.js"
+  },
+  {
+    "revision": "e5dd81403b7e391f369b",
+    "url": "/static/js/2.b15d1b0e.chunk.js"
+  },
+  {
+    "revision": "5ec5ed894fef17538a8a",
+    "url": "/static/css/main.04c5859c.chunk.css"
+  },
+  {
+    "revision": "6f381ceaf10ce1c4ccbc067da7979020",
+    "url": "/index.html"
+  }
+];
