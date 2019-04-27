a69b43fae61f418e461c3f0987685759d26bd232
Denis Savisko <42087984+DenisSavisko@users.noreply.github.com>
2019-04-28 01:36:13 +0300

    eslint allow html in .js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,25 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'jest': true,
-    'es6': true,
-    'node': true,
+  env: {
+    browser: true,
+    jest: true,
+    es6: true,
+    node: true,
   },
-  'extends': [
-    'airbnb',
-    'prettier',
-  ],
-  'plugins': [
-    'prettier',
-  ],
-  'rules': {
-    'prettier/prettier': ['error', {
-      'singleQuote': true,
-      'trailingComma': 'es5'
-    }],
+  extends: ['airbnb', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+      },
+    ],
   },
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
-    }
-  }
-}
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+};
